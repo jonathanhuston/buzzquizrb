24f69e41dfcfcd225ae4002c3c767848bd04e6e6
Jonathan Huston <jonathanhuston@icloud.com>
2018-10-25 15:04:22 +0200

    refactoring, quit and play again buttons

--- buzzquiz.rb
+++ buzzquiz.rb
@@ -55,11 +55,14 @@ end
 
 def display_character(character)
   root = TkRoot.new {title "#{character[:name]}"}
-  content = Tk::Tile::Frame.new(root) {padding "20"}.grid(:column => 0, :row => 0, :sticky => 'nsew')
-  Tk::Tile::Label.new(content) {text "You are #{character[:name]}."; foreground character[:color]}.grid(:column => 0, :row => 1, :sticky => 'w')
-  Tk::Tile::Label.new(content) {text character[:description]; foreground character[:color]}.grid(:column => 0, :row => 2, :sticky => 'w')
+  content = Tk::Tile::Frame.new(root) {padding "20"}.grid(:column => 0, :row => 0)
+  Tk::Tile::Label.new(content) {text "You are #{character[:name]}."; foreground character[:color]}.grid(:column => 0, :row => 0, :sticky => 'w')
+  Tk::Tile::Label.new(content) {text character[:description]; foreground character[:color]}.grid(:column => 0, :row => 1, :sticky => 'w')
   image = TkPhotoImage.new(:file => character[:image])
-  Tk::Tile::Label.new(content) {image image}.grid(:column => 0, :row => 3, :sticky => 'w')
+  Tk::Tile::Label.new(content) {image image}.grid(:column => 0, :row => 2, :sticky => 'w')
+  Tk::Tile::Frame.new(content) {height 60}.grid(:column => 0, :row => 3)
+  Tk::Tile::Button.new(content) {text "Play again?"; command "run_quiz"}.grid(:column => 0, :row => 3, :sticky => 'w')
+  Tk::Tile::Button.new(content) {text "Quit"; command 'exit'}.grid(:column => 0, :row => 3, :sticky => 'e')
 end
 
 def helper(index)
@@ -76,8 +79,8 @@ end
 
 def display_questions(index)
   root = TkRoot.new {title $title}
-  content = Tk::Tile::Frame.new(root) {padding "20 20 0 20"}.grid(:column => 0, :row => 0, :sticky => 'nsew')
-  Tk::Tile::Frame.new(content) {width 400; height 40}.grid(:column => 0, :row => 0, :sticky => 'nsew')
+  content = Tk::Tile::Frame.new(root) {padding "20 20 0 20"}.grid(:column => 0, :row => 0)
+  Tk::Tile::Frame.new(content) {width 400; height 40}.grid(:column => 0, :row => 0, :sticky => 'w')
   Tk::Tile::Label.new(content) {text $questions[index][:question]}.grid(:column => 0, :row => 0, :sticky => 'w')
   $answer = TkVariable.new
   row = 1
@@ -85,9 +88,15 @@ def display_questions(index)
     Tk::Tile::RadioButton.new(content) {text answer; variable $answer; value row}.grid(:column => 0, :row => row, :sticky => 'w')
     row += 1
   end
-  Tk::Tile::Frame.new(content) {width 400; height 60}.grid(:column => 0, :row => row, :sticky => 'w')
+  Tk::Tile::Frame.new(content) {width 400; height 60}.grid(:column => 0, :row => row)
   Tk::Tile::Button.new(content) {text "Next"; command "helper #{index}"}.grid(:column => 0, :row => row, :sticky => 'w')
   Tk.mainloop
+end
+
+def run_quiz
+  TkRoot.new {geometry "+300+150"}
+  $point_totals = Array.new($descriptions.size, 0)
+  display_questions(0)
 end
 
 quiz_name = ARGV[0]
@@ -96,13 +105,10 @@ begin
   descriptions_file = "#{Dir.home}/quiz_data/#{quiz_name.tr(' ', '-')}-descriptions.csv"
   images_folder = "#{Dir.home}/quiz_data/#{quiz_name.tr(' ', '-')}-images/"
   $title, $questions, $descriptions = load_quiz(questions_file, descriptions_file, images_folder)
-
-  $point_totals = Array.new($descriptions.size, 0)
 rescue
   puts "Game engine unable to find properly formatted game data."
   puts "Check command line argument and game data files."
   exit
 end
 
-root = TkRoot.new {geometry "+300+150"}
-display_questions(0)
+run_quiz
